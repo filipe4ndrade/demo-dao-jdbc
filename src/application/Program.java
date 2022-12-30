b203bfbe4352ff95df5773cea7d7fe02aa9677f5
Filipe <filipeandrade000@gmail.com>
2022-12-30 04:10:02 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,12 +17,21 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
         System.out.println();
+        
 		System.out.println("=== TEST 2: seller findByDepartmentId ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println();
+		
+		System.out.println("=== TEST 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		System.out.println();
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -98,8 +98,52 @@ public class SellerDaoJDBC implements SellerDAO {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		String sql = "SELECT seller.*,department.Name as DepName "+
+				"FROM seller INNER JOIN department "+
+				"ON seller.DepartmentId = department.Id "+
+				"ORDER BY Name";
+		try {
+			st = this.conn.prepareStatement(sql);
+			rs = st.executeQuery();
+
+			List<Seller> list = new ArrayList<>();
+			//Map necessário para evitar a repetição de dep
+			Map<Integer, Department> map = new HashMap<>();
+			
+			// verificar se tem id
+			while (rs.next()) {
+				
+				//verifica se existe o departmentId
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs,dep);
+				list.add(obj);
+			
+
+			}
+
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
+		finally {
+			try {
+				rs.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 	@Override
